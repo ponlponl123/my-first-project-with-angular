bc90eb116d8b07ac3703d23bb165b829900f8662
ponlponl123 <pattarapong.sowcharoen@gmail.com>
2025-06-05 11:57:11 +0700

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { App } from './app';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: App
+    },
+    {
+        path: 'about',
+        component: About
+    }
+];
